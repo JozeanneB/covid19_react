067df463a7c19ae58a470b100414aa6e92fccf5d
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2021-08-29 22:29:29 -0300

    Adiciona container Main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { StylesProvider } from "@material-ui/styles";
 import { CssBaseline } from "@material-ui/core";
-import GlobalStyle from "./common/styles/global-style";
+import GlobalStyle from "./commons/styles/global-style";
+import Main from "./containers/Main/index";
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
-      <div>teste</div>
+      <Main />
     </StylesProvider>
   );
 }
